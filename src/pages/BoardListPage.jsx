13ff81185c66b31ca96eb9aa2cf40560c64c147b
moonjoonbong <moonjoon1212@gmail.com>
2026-07-08 10:49:19 +0900

    feat : page, search 스테이트 값이 변경되면 axios로 백엔드를 호출하여 게시글 목록 + 페이징 정보를 받아오도록 했고, 받아온 게시글을 PostCard 컴포넌트로 출력, 현재 페이지 번호, 전체 페이지 번호, 페이지 이동하는 함수를 props로 넘겨주어 페이지네이션이 구동되도록 함.

--- src/pages/BoardListPage.jsx
+++ src/pages/BoardListPage.jsx
@@ -1,10 +1,55 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
+import api from '../api/axios';
+import SearchBar from '../components/SearchBar';
+import Pagenation from '../components/Pagenation';
 
 const BoardListPage = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false); // 데이터를 로딩중(통신중)인지 아닌지
+    const [error, setError] = useState(null); // 에러 메세지를 출력 할지 말지
+    const [posts, setPosts] = useState([]); // 게시글 데이터
+    const [page, setPage] = useState(1); // 현재 페이지 번호
+    const [search, setSearch] = useState(''); // backend를 호출 했던 검색어
+    const [tempSearch, setTempSearch] = useState(''); // 아직 입력중인 검색어(backend 호출x)
+    const [totalPages, setTotalPages] = useState(1); // 전체 페이지 수
+
+    const fetchPosts = async () => {
+        setLoading(true); // 로딩 시작
+        setError(null); // 이전 에러 초기화
+        try {
+            const response = await api.get('/posts', {
+                params: {
+                    page,
+                    per_page: 10,
+                    ...(search && { search }), // search가 있을 때만 파라미터 추가
+                },
+            });
+
+            setPosts(response.data.posts);
+            setTotalPages(response.data.page_info.total_pages);
+        } catch (err) {
+            setError('게시글을 불러는데 실패했습니다..');
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    // page(페이지 번호), search(검색어)가 바뀔 때마다 호출 되도록
+    // 최초에 컴포넌트가 렌더링 될 때 page 스테이트 값이 1로 초기화 되면서
+    // 아래의 fetchPosts()가 호출 된다.
+    useEffect(() => {
+        fetchPosts();
+    }, [page, search]);
+
+    const handelSearch = () => {
+        setSearch(tempSearch);
+        setPage(1);
+    };
+
+    const handelSearchKeyDown = (e) => {
+        if (e.key === 'Enter') handelSearch();
+    };
 
     return (
         <div className="board-list-page">
@@ -12,6 +57,13 @@ const BoardListPage = () => {
                 <h1>게시판</h1>
                 <button>글쓰기</button>
             </div>
+
+            <SearchBar
+                value={tempSearch}
+                onChange={(e) => setTempSearch(e.target.value)}
+                onSearch={handelSearch}
+                onKeyDown={handelSearchKeyDown}
+            />
 
             {/* 로딩중 표시 */}
             {loading && <div className="loading"> 불러오는중 </div>}
@@ -36,6 +88,12 @@ const BoardListPage = () => {
                     )}
                 </>
             )}
+            {/* 페이지네이션 */}
+            <Pagenation
+                page={page}
+                totalPages={totalPages}
+                onPageChange={(newPage) => setPage(newPage)}
+            />
         </div>
     );
 };
