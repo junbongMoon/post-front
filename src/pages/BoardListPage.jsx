ebbc22774b8a3dd8eae7577ff2739627976f1827
moonjoonbong <moonjoon1212@gmail.com>
2026-07-14 11:38:22 +0900

    feat : useReducer()를 이용해서 리팩토링 + useCallback()으로 최적화

--- src/pages/BoardListPage.jsx
+++ src/pages/BoardListPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import PostCard from '../components/PostCard';
@@ -6,14 +6,63 @@ import api from '../api/axios';
 import SearchBar from '../components/SearchBar';
 import Pagenation from '../components/Pagenation';
 
+// ----------------- Reducer를 컴포넌트 바깥에 정의 (렌더링 될때마다 재생성 방지)
+const ACTION = {
+    FETCH_START: 'FETCH_START', // 데이터 요청시작
+    FETCH_SUCCESS: 'FETCH_SUCCESS', // 데이터 요청 성공
+    FETCH_ERROR: 'FETCH_ERROR', // 데이터 요청 실패
+    SET_PAGE: 'SET_PAGE', // 페이지 변경
+    SET_SEARCH: 'SET_SEARCH', // 검색어 변경
+};
+
+const initialState = {
+    posts: [],
+    loading: false,
+    error: null,
+    page: 1,
+    search: '',
+    pageInfo: null,
+}; // 각 state의 초기값
+
+function boardReducer(state, action) {
+    switch (action.type) {
+        case ACTION.FETCH_START:
+            return { ...state, loading: true, error: null };
+        case ACTION.FETCH_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                posts: action.payload.posts,
+                pageInfo: action.payload.pageInfo,
+            };
+        case ACTION.FETCH_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                posts: [],
+            };
+        case ACTION.SET_PAGE:
+            return { ...state, page: action.payload };
+        case ACTION.SET_SEARCH:
+            return { ...state, search: action.payload, page: 1 };
+
+        default: // 위 case의 경우가 아니라면...
+            return state;
+    }
+}
+
 const BoardListPage = () => {
-    const [loading, setLoading] = useState(false); // 데이터를 로딩중(통신중)인지 아닌지
-    const [error, setError] = useState(null); // 에러 메세지를 출력 할지 말지
-    const [posts, setPosts] = useState([]); // 게시글 데이터
-    const [page, setPage] = useState(1); // 현재 페이지 번호
-    const [search, setSearch] = useState(''); // backend를 호출 했던 검색어
+    // const [loading, setLoading] = useState(false); // 데이터를 로딩중(통신중)인지 아닌지
+    // const [error, setError] = useState(null); // 에러 메세지를 출력 할지 말지
+    // const [posts, setPosts] = useState([]); // 게시글 데이터
+    // const [page, setPage] = useState(1); // 현재 페이지 번호
+    // const [search, setSearch] = useState(''); // backend를 호출 했던 검색어
     const [tempSearch, setTempSearch] = useState(''); // 아직 입력중인 검색어(backend 호출x)
-    const [pageInfo, setPageInfo] = useState(null); // 전체 페이지 수
+    // const [pageInfo, setPageInfo] = useState(null); // 페이지 정보
+
+    const [state, dispatch] = useReducer(boardReducer, initialState);
+    const { posts, loading, error, page, search, pageInfo } = state;
 
     const navigate = useNavigate(); // 페이지 이동을 할 수 있도록 하는 react-router-dom의 훅
 
@@ -23,8 +72,10 @@ const BoardListPage = () => {
 
         // useEffect의 의존성 배열에 함수가 들어간다. => 그 함수를 useCallback!
 
-        setLoading(true); // 로딩 시작
-        setError(null); // 이전 에러 초기화
+        // setLoading(true); // 로딩 시작
+        // setError(null); // 이전 에러 초기화
+        dispatch({ type: ACTION.FETCH_START });
+
         try {
             const response = await api.get('/posts', {
                 params: {
@@ -36,15 +87,21 @@ const BoardListPage = () => {
 
             console.log(response.data.page_info);
 
-            setPosts(response.data.posts);
-            setPageInfo(response.data.page_info);
+            // setPosts(response.data.posts);
+            // setPageInfo(response.data.page_info);
+            dispatch({
+                type: ACTION.FETCH_SUCCESS,
+                payload: {
+                    posts: response.data.posts,
+                    pageInfo: response.data.page_info,
+                },
+            });
 
-            console.log(response.data.page_info);
+            // console.log(response.data.page_info);
         } catch (err) {
-            setError('게시글을 불러는데 실패했습니다..');
+            // setError('게시글을 불러는데 실패했습니다..');
+            dispatch({ type: ACTION.FETCH_ERROR, payload: '게시글을 불러는데 실패했습니다..' });
             console.log(err);
-        } finally {
-            setLoading(false);
         }
     }, [page, search]);
 
@@ -55,14 +112,19 @@ const BoardListPage = () => {
         fetchPosts();
     }, [page, search]);
 
-    const handelSearch = () => {
-        setSearch(tempSearch);
-        setPage(1);
-    };
+    const handelSearch = useCallback(() => {
+        // setSearch(tempSearch);
+        // setPage(1);
+        dispatch({ type: ACTION.SET_SEARCH, payload: tempSearch });
+    }, [tempSearch]);
 
     const handelSearchKeyDown = (e) => {
         if (e.key === 'Enter') handelSearch();
     };
+
+    const handelPageChange = useCallback((newPage) => {
+        dispatch({ type: ACTION.SET_PAGE, payload: newPage });
+    }, []); // 의존성 없음 -> 처음 1회만 생성 이후 재사용
 
     return (
         <div className="board-list-page">
@@ -104,7 +166,10 @@ const BoardListPage = () => {
             )}
             {/* 페이지네이션 */}
             {pageInfo && (
-                <Pagenation pageInfo={pageInfo} onPageChange={(newPage) => setPage(newPage)} />
+                <Pagenation
+                    pageInfo={pageInfo}
+                    onPageChange={(newPage) => handelPageChange(newPage)}
+                />
             )}
         </div>
     );
